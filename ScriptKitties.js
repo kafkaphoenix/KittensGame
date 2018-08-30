b8422a4aba8d0d3be1661bd0ff7b3325ebad7bb3
kafkaphoenix <jaguilerapuerta@gmail.com>
2018-08-30 10:06:40 +0200

    Update ScriptKitties.js
    Autocraft of fur derivatives added

--- ScriptKitties.js
+++ ScriptKitties.js
@@ -1,26 +1,40 @@
+var furDerVal = 2;
+
 // Auto Observe Astronomical Events
 
 function autoObserve() {
-  var checkObserveBtn = document.getElementById("observeBtn");
-		if (typeof(checkObserveBtn) != 'undefined' && checkObserveBtn != null) {
-			checkObserveBtn.click();				
-		}
+	var checkObserveBtn = document.getElementById("observeBtn");
+	if (typeof(checkObserveBtn) != 'undefined' && checkObserveBtn != null) {
+		checkObserveBtn.click();				
+	}
 }
 
 // Auto Hunt
 
 function autoHunt() {
 	var catpower = gamePage.resPool.get('manpower');
-		if (catpower.value > (catpower.maxValue - 1)) {
-			gamePage.village.huntAll();
+	if (catpower.value > (catpower.maxValue - 1)) {
+		gamePage.village.huntAll();
+	}
+}
+
+function autoCraft() {
+	
+//Craft the fur derivatives
+var furDerivatives = ['parchment', 'manuscript', 'compedium', 'blueprint'];
+	for (var i = 0; i < furDerVal; i++) {
+  		if (gamePage.workshop.getCraft(furDerivatives[i]).unlocked) { 
+				gamePage.craftAll(furDerivatives[i]); 
 		}
+	}
 }	
 }
 
 clearInterval(runAllAutomation);
 var runAllAutomation = setInterval(function() {  
-  if (gamePage.timer.ticksTotal % 3 === 0) {
-	autoObserve();
-	autoHunt();
-}  
+	if (gamePage.timer.ticksTotal % 3 === 0) {
+		autoObserve();
+		autoHunt();
+		autoCraft();
+	}  
 }, 200);
