86dca6e96f8531279b2bea317558760ba280511f
kafkaphoenix <jaguilerapuerta@gmail.com>
2018-09-10 22:07:57 +0200

    Update AutoKittens.js
    Added auto Space Buildings and Programs (Finished)

--- AutoKittens.js
+++ AutoKittens.js
@@ -11,6 +11,7 @@ var praiseMsg = "Auto Praise activated!";
 var scienceMsg = "Auto Science activated!";
 var upgradeMsg = "Auto Upgrade activated!";
 var partyMsg = "Party time!"
+var spaceMsg = "Auto Space activated!";
 var nm = 0;
 var at = 0;
 var ac = 0;
@@ -21,7 +22,9 @@ var ap = 0;
 var as = 0;
 var au = 0;
 var apr = 0;
+var aspace = 0;
 var steamOn = 0;
+var programBuild = false;
 
 var buildings = [
         ["Field", false], 
@@ -203,7 +206,7 @@ var htmlMenuAddition = '<div id="autokittens" class="column">' +
 
 '<a id="scriptOptions" onclick="openMenu()"> | AutoKittens </a>' + 
 
-'<div id="menu" style="display:none; margin-top:-420px; margin-left:-100px; width:220px; height:345px !important" class="dialog help">' + 
+'<div id="menu" style="display:none; margin-top:-450px; margin-left:-100px; width:220px; height:375px !important" class="dialog help">' + 
 '<a href="#" class="close" onclick="closeMenu();" style="position: absolute; top: 10px; right: 15px;">close</a>' + 
 
 '<input type="button" value="Stop Script" style="position: absolute; left: 15px;" id="stopScript" onclick="clearInterval(clearScript()); gamePage.msg(deadScript);">' +
@@ -211,19 +214,22 @@ var htmlMenuAddition = '<div id="autokittens" class="column">' +
 
 '<input type="button" value="AutoTrade" style="position: absolute; left: 15px; top: 100px; width: 130px;" id="autoTrade" onclick="switchAutoTrade(); gamePage.msg(tradeMsg);">' +      
 '<input type="button" value="AutoBuild" style="position: absolute; left: 15px; top: 130px; width: 130px;" id="autoBuild" onclick="switchAutoBuild(); gamePage.msg(buildMsg);">' +      
-'<input type="button" value="AutoCraft" style="position: absolute; left: 15px; top: 160px; width: 130px;" id="autoCraft" onclick="switchAutoCraft(); gamePage.msg(craftMsg);">' +      
-'<input type="button" value="AutoKittens" style="position: absolute; left: 15px; top: 190px; width: 130px;" id="autoKittens" onclick="switchAutoKittens(); gamePage.msg(kittensMsg);">' +  
-'<input type="button" value="AutoHunt" style="position: absolute; left: 15px; top: 220px; width: 130px;" id="autoHunt" onclick="switchAutoHunt(); gamePage.msg(huntMsg);">' +  
-'<input type="button" value="AutoPraise" style="position: absolute; left: 15px; top: 250px; width: 130px;" id="autoPraise" onclick="switchAutoPraise(); gamePage.msg(praiseMsg);">' +      
-'<input type="button" value="AutoParty" style="position: absolute; left: 15px; top: 280px; width: 130px;" id="autoParty" onclick="switchAutoParty(); gamePage.msg(partyMsg);">' +  
-'<input type="button" value="AutoUpgrade" style="position: absolute; left: 15px; top: 310px; width: 130px;" id="autoUpgrade" onclick="switchAutoUpgrade(); gamePage.msg(upgradeMsg);">' +  
-'<input type="button" value="AutoScience" style="position: absolute; left: 15px; top: 340px; width: 130px;" id="autoScience" onclick="switchAutoScience(); gamePage.msg(scienceMsg);">' +      
+'<input type="button" value="AutoSpace" style="position: absolute; left: 15px; top: 160px; width: 130px;" id="autoSpace" onclick="switchAutoSpace(); gamePage.msg(spaceMsg);">' +      
+'<input type="button" value="AutoCraft" style="position: absolute; left: 15px; top: 190px; width: 130px;" id="autoCraft" onclick="switchAutoCraft(); gamePage.msg(craftMsg);">' +      
+'<input type="button" value="AutoKittens" style="position: absolute; left: 15px; top: 220px; width: 130px;" id="autoKittens" onclick="switchAutoKittens(); gamePage.msg(kittensMsg);">' +  
+'<input type="button" value="AutoHunt" style="position: absolute; left: 15px; top: 250px; width: 130px;" id="autoHunt" onclick="switchAutoHunt(); gamePage.msg(huntMsg);">' +  
+'<input type="button" value="AutoPraise" style="position: absolute; left: 15px; top: 280px; width: 130px;" id="autoPraise" onclick="switchAutoPraise(); gamePage.msg(praiseMsg);">' +      
+'<input type="button" value="AutoParty" style="position: absolute; left: 15px; top: 310px; width: 130px;" id="autoParty" onclick="switchAutoParty(); gamePage.msg(partyMsg);">' +  
+'<input type="button" value="AutoUpgrade" style="position: absolute; left: 15px; top: 340px; width: 130px;" id="autoUpgrade" onclick="switchAutoUpgrade(); gamePage.msg(upgradeMsg);">' +  
+'<input type="button" value="AutoScience" style="position: absolute; left: 15px; top: 370px; width: 130px;" id="autoScience" onclick="switchAutoScience(); gamePage.msg(scienceMsg);">' +      
+
 
 
 '<input type="button" value="+" style="position: absolute; left: 150px; top: 100px; width: auto;" id="autoTradingOptions" onclick="openAToptions();">' +      
 '<input type="button" value="+" style="position: absolute; left: 150px; top: 130px; width: auto;" id="autoBuildingOptions" onclick="openABoptions();">' +    
-'<input type="button" value="+" style="position: absolute; left: 150px; top: 160px; width: auto;" id="autoCraftingOptions" onclick="openACoptions();">' +   
-'<input type="button" value="+" style="position: absolute; left: 150px; top: 190px; width: auto;" id="autoKittensOptions" onclick="openAKoptions();">' +
+'<input type="button" value="+" style="position: absolute; left: 150px; top: 160px; width: auto;" id="autoSpaceOptions" onclick="openABSoptions();">' +    
+'<input type="button" value="+" style="position: absolute; left: 150px; top: 190px; width: auto;" id="autoCraftingOptions" onclick="openACoptions();">' +   
+'<input type="button" value="+" style="position: absolute; left: 150px; top: 220px; width: auto;" id="autoKittensOptions" onclick="openAKoptions();">' +
     
 '</div>' +
 '</div>'
@@ -231,7 +237,7 @@ var htmlMenuAddition = '<div id="autokittens" class="column">' +
 $("#footerLinks").append(htmlMenuAddition);
 
 var bldSelectAddition = '<div id="menuAB" style="display:none; margin-top:-360px; width:200px; z-index: 1;" class="dialog help">' + 
-'<a href="#" onclick="$(\'#menuASpace\').toggle(); $(\'#menuAB\').hide();" style="position: absolute; top: 10px; left: 15px;">space</a>' + 
+'<a href="#" onclick="$(\'#menuAB\').hide(); $(\'#menuASpace\').toggle();" style="position: absolute; top: 10px; left: 15px;">Space</a>' + 
 '<a href="#" onclick="$(\'#menuAB\').hide();" style="position: absolute; top: 10px; right: 15px;">close</a>' + 
 
 '	<br><input type="checkbox" id="prodChecker"><label for="prodChecker" onclick="$(\'.prodCheck\').click();"><b>Food Production</b></label><br>' + 
@@ -289,56 +295,55 @@ var bldSelectAddition = '<div id="menuAB" style="display:none; margin-top:-360px
 '</div>'
 
 var spaceSelectAddition = '<div id="spaceSelect" style="display:none; margin-top:-360px; width:200px; z-index: 1;" class="dialog help">' + 
-'<a href="#" onclick="$(\'#menuASpace\').hide(); $(\'#menuAB\').toggle();" style="position: absolute; top: 10px; left: 15px;">cath</a>' + 
+'<a href="#" onclick="$(\'#menuASpace\').hide(); $(\'#menuAB\').toggle();" style="position: absolute; top: 10px; left: 15px;">Cath</a>' + 
 '<a href="#" onclick="$(\'#menuASpace\').hide();" style="position: absolute; top: 10px; right: 15px;">close</a>' + 
 
-'	</br><input type="checkbox" id="programs" class="programs" onchange="programBuild = this.checked; console.log(this.checked);"><label for="programs">Programs</label></br></br>' + 
+'	</br><input type="checkbox" id="programs" class="programs" onchange="programBuild = this.checked;"><label for="programs">Programs</label></br></br>' + 
 
 '	<input type="checkbox" id="spaceChecker"><label for="spaceChecker" onclick="$(\'.spaceCheck\').click();"><b>Space</b></label></br>' + 
-
-'	<input type="checkbox" id="elevSBld" class="spaceCheck" onchange="verifyBuildingSelected(\'36\', \'elevSBld\');"><label for="elevSBld">Space Elevator</label></br>' + 
-'	<input type="checkbox" id="satSBld" class="spaceCheck" onchange="verifyBuildingSelected(\'33\', \'satSBld\');"><label for="satSBld">Satellite</label></br>' + 
-'	<input type="checkbox" id="statSBld" class="spaceCheck" onchange="verifyBuildingSelected(\'34\', \'statSBld\');"><label for="statSBld">Space Station</label></br></br>' + 
+'	<input type="checkbox" id="elevSBld" class="spaceCheck" onchange="verifyBuildingSelected(\'34\', \'elevSBld\');"><label for="elevSBld">Space Elevator</label></br>' + 
+'	<input type="checkbox" id="satSBld" class="spaceCheck" onchange="verifyBuildingSelected(\'35\', \'satSBld\');"><label for="satSBld">Satellite</label></br>' + 
+'	<input type="checkbox" id="statSBld" class="spaceCheck" onchange="verifyBuildingSelected(\'36\', \'statSBld\');"><label for="statSBld">Space Station</label></br></br>' + 
 
 '	<input type="checkbox" id="moonChecker"><label for="moonChecker" onclick="$(\'.moonCheck\').click();"><b>Moon</b></label></br>' + 
-
 '	<input type="checkbox" id="outSBld" class="moonCheck" onchange="verifyBuildingSelected(\'35\', \'outSBld\');"><label for="outSBld">Lunar Outpost</label></br>' + 
 '	<input type="checkbox" id="baseSBld" class="moonCheck" onchange="verifyBuildingSelected(\'36\', \'baseSBld\');"><label for="baseSBld">Moon Base</label></br></br>' + 
 
 '	<input type="checkbox" id="duneChecker"><label for="duneChecker" onclick="$(\'.duneCheck\').click();"><b>Dune</b></label></br>' + 
-
-
 '	<input type="checkbox" id="crackSBld" class="duneCheck" onchange="verifyBuildingSelected(\'37\', \'crackSBld\');"><label for="crackSBld">Planet Cracker</label></br>' + 
-'	<input type="checkbox" id="fracSBld" class="duneCheck" onchange="verifyBuildingSelected(\'38\', \'fracSBld\');"><label for="fracSBld">Hydro Fracturer</label></br>' + 
+'	<input type="checkbox" id="fracSBld" class="duneCheck" onchange="verifyBuildingSelected(\'38\', \'fracSBld\');"><label for="fracSBld">Hydraulic Fracturer</label></br>' + 
 '	<input type="checkbox" id="spiceSBld" class="duneCheck" onchange="verifyBuildingSelected(\'39\', \'spiceSBld\');"><label for="spiceSBld">Spice Refinery</label></br></br>' + 
 
 '	<input type="checkbox" id="piscineChecker"><label for="piscineChecker" onclick="$(\'piscineCheck\').click();"><b>Piscine</b></label></br>' + 
-
 '	<input type="checkbox" id="reVeSBld" class="piscineCheck" onchange="verifyBuildingSelected(\'40\', \'reVeSBld\');"><label for="reVeSBld">Research Vessel</label></br>' + 
 '	<input type="checkbox" id="orbSBld" class="piscineCheck" onchange="verifyBuildingSelected(\'41\', \'orbSBld\');"><label for="orbSBld">Orbital Array</label></br></br>' + 
 
 '	<input type="checkbox" id="heliosChecker"><label for="heliosChecker" onclick="$(\'.heliosCheck\').click();"><b>Helios</b></label></br>' + 
-
 '	<input type="checkbox" id="sunSBld" class="heliosCheck" onchange="verifyBuildingSelected(\'42\', \'sunSBld\');"><label for="sunSBld">Sunlifter</label></br>' + 
 '	<input type="checkbox" id="contSBld" class="heliosCheck" onchange="verifyBuildingSelected(\'43\', \'contSBld\');"><label for="contSBld">Containment Chamber</label></br></br>' + 
+'	<input type="checkbox" id="heatSinkSBld" class="heliosCheck" onchange="verifyBuildingSelected(\'43\', \'heatSinkSBld\');"><label for="heatSinkSBld">Heatsink</label></br></br>' + 
+'	<input type="checkbox" id="sunForgeSBld" class="heliosCheck" onchange="verifyBuildingSelected(\'43\', \'sunForgeSBld\');"><label for="sunForgeSBld">Sunforge</label></br></br>' + 
 
 '	<input type="checkbox" id="terminusChecker"><label for="terminusChecker" onclick="$(\'.terminusCheck\').click();"><b>Terminus</b></label></br>' + 
-
 '	<input type="checkbox" id="crySBld" class="terminusCheck" onchange="verifyBuildingSelected(\'44\', \'crySBld\');"><label for="crySBld">Cryostation</label></br></br>' + 
 
 '	<input type="checkbox" id="kairoChecker"><label for="kairoChecker" onclick="$(\'.kairoCheck\').click();"><b>Kairo</b></label></br>' + 
-
 '	<input type="checkbox" id="beacSBld" class="kairoCheck" onchange="verifyBuildingSelected(\'45\', \'beacSBld\');"><label for="beacSBld">Space Beacon</label></br></br>' + 
 
 '	<input type="checkbox" id="yarnChecker"><label for="yarnChecker" onclick="$(\'.yarnCheck\').click();"><b>Yarn</b></label></br>' + 
-
 '	<input type="checkbox" id="terrSBld" class="yarnCheck" onchange="verifyBuildingSelected(\'46\', \'terrSBld\');"><label for="terrSBld">Terraforming Station</label></br>' + 
 '	<input type="checkbox" id="hydrSBld" class="centaurusCheck" onchange="verifyBuildingSelected(\'47\', \'hydrSBld\');"><label for="hydrSBld">Hydroponics</label></br></br>' + 
 
-'	<input type="checkbox" id="centaurusChecker"><label for="centaurusChecker" onclick="$(\'.centaurusCheck\').click();"><b>Centaurus System</b></label></br>' + 
+'	<input type="checkbox" id="umbraChecker"><label for="umbraChecker" onclick="$(\'.umbraCheck\').click();"><b>Umbra</b></label></br>' + 
+'	<input type="checkbox" id="hrSBld" class="umbraCheck" onchange="verifyBuildingSelected(\'48\', \'hrSBld\');"><label for="hrSBld">HR Harvester</label></br></br>' + 
 
+'	<input type="checkbox" id="charonChecker"><label for="charonChecker" onclick="$(\'.charonCheck\').click();"><b>Charon</b></label></br>' + 
+'	<input type="checkbox" id="esSBld" class="charonCheck" onchange="verifyBuildingSelected(\'48\', \'esSBld\');"><label for="esSBld">Entanglement Station</label></br></br>' + 
+
+'	<input type="checkbox" id="centaurusChecker"><label for="centaurusChecker" onclick="$(\'.centaurusCheck\').click();"><b>Centaurus System</b></label></br>' + 
 '	<input type="checkbox" id="tecSBld" class="centaurusCheck" onchange="verifyBuildingSelected(\'48\', \'tecSBld\');"><label for="tecSBld">Tectonic</label></br></br>' + 
 
+'	<input type="checkbox" id="furthestRingChecker"><label for="furthestRingChecker" onclick="$(\'.furthestRingCheck\').click();"><b>Furthest Ring</b></label></br>' +
 '</div>'
 
 function verifyBuildingSelected(buildingNumber, buildingCheckID) {
@@ -347,6 +352,7 @@ function verifyBuildingSelected(buildingNumber, buildingCheckID) {
 }
 
 $("#game").append(bldSelectAddition);
+$("#game").append(spaceSelectAddition);
 
 function closeMenu() {
 	$("#menu").hide();
@@ -362,6 +368,10 @@ function openAToptions() {
 
 function openABoptions() {
 	$("#menuAB").toggle();
+}
+
+function openABSoptions() {
+	$("#menuASpace").toggle();
 }
 
 function openACoptions() {
@@ -529,6 +539,77 @@ function switchAutoBuild()
 	}	
 }
 
+		// Build space stuff automatically
+function autoSpace() {		
+if (autoCheck[0] != "false") {	
+	
+	var origTab = gamePage.ui.activeTabId;	
+		
+		// Build space buildings
+	for (var z = 34; z < buildings.length; z++) {
+		if (buildings[z][1] != false) {		
+			
+		var spBuild = gamePage.tabs[6].planetPanels[buildings[z][2]].children;
+		
+			try { 			
+				for (i = 0 ;i < spBuild.length; i++) {
+					if (spBuild[i].model.metadata.name == buildingsList[z]) {
+						
+						if (gamePage.ui.activeTabId != "Space") {
+							gamePage.ui.activeTabId = 'Space'; gamePage.render(); // Change the tab so that we can build
+						}
+						
+						spBuild[i].controller.buyItem(spBuild[i].model, {}, function(result) {
+							if (result) {spBuild[i].update();}
+							});
+					} 
+				}		
+			} catch(err) {
+			console.log(err);
+			}
+			
+		}
+	}
+	
+		// Build space programs
+	if (programBuild != false) {
+		var spcProg = gamePage.tabs[6].GCPanel.children;
+		for (var i = 0; i < spcProg.length; i++) {
+			if (spcProg[i].model.metadata.unlocked && spcProg[i].model.on == 0) {
+				try { 		
+					
+					if (gamePage.ui.activeTabId != "Space") {
+					gamePage.ui.activeTabId = 'Space'; gamePage.render(); // Change the tab so that we can build
+					}
+					
+					spcProg[i].controller.buyItem(spcProg[i].model, {}, function(result) {
+						if (result) {spcProg[i].update();}
+						});
+				} catch(err) {
+				console.log(err);
+				}
+			}
+		}
+	}
+	
+	      if (origTab != gamePage.ui.activeTabId) {
+        gamePage.ui.activeTabId = origTab; gamePage.render(); // Return to the original tab
+		  }
+	
+}
+}			
+
+function switchAutoSpace()
+{
+	aspace = aspace == 0 ? 1 : 0;
+	
+	if (aspace == 1) {
+		spaceMsg = "Auto Space activated!";
+	} else {
+		spaceMsg = "Auto Space deactivated!";
+	}	
+}
+
 // Festival automatically
 
 function autoParty() {
@@ -663,6 +744,9 @@ var runAllAutomation = setInterval(function() {
 	}
 	if (au == 1){
 		autoUpgrade();
+	}
+	if (aspace == 1){
+		autoSpace();
 	}
 	
 	//day
